6008b2b918f1b69404475e648f9c112e215868a1
Ahnaf Talukder <talukderaxif@gmail.com>
2025-03-08 14:47:08 -0500

    Setting up Github

---
+++ jackal_ws/build/CTestTestfile.cmake
@@ -0,0 +1,18 @@
+# CMake generated Testfile for 
+# Source directory: /home/ahnaf/BARN/jackal_ws/src
+# Build directory: /home/ahnaf/BARN/jackal_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("jackal_desktop/jackal_desktop")
+subdirs("jackal_simulator/jackal_simulator")
+subdirs("jackal/jackal_tutorials")
+subdirs("jackal/jackal_msgs")
+subdirs("jackal/jackal_control")
+subdirs("jackal/jackal_description")
+subdirs("jackal_simulator/jackal_gazebo")
+subdirs("jackal/jackal_navigation")
+subdirs("jackal_desktop/jackal_viz")
+subdirs("the-barn-challenge/jackal_helper")
+subdirs("eband_local_planner")
---
+++ jackal_ws/build/the-barn-challenge/jackal_helper/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ahnaf/BARN/jackal_ws/src/the-barn-challenge/jackal_helper
+# Build directory: /home/ahnaf/BARN/jackal_ws/build/the-barn-challenge/jackal_helper
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
